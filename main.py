c9c7a42a89483254bf240776670173d0d0b6752e
bblachut1 <bogojedi1@gmail.com>
2022-04-08 00:49:26 +0200

    refactor main

--- main.py
+++ main.py
@@ -6,132 +6,135 @@ from fastai.learner import load_learner
 from minizinc import Instance, Model, Solver
 from fastbook import *
 
-image = cv2.imread("sudoku2.jpeg")
-cv2.imshow("Image", image)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Image", gray)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-cv2.imshow("Image", blur)
-thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
-cv2.imshow("Image", thresh)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+def main():
+    image = cv2.imread("sudoku.jpg")
+    cv2.imshow("Image", image)
 
-max_area = 0
-c = 0
-for i in contours:
-    area = cv2.contourArea(i)
-    if area > 10000:
-        if area > max_area:
-            max_area = area
-            best_cnt = i
-            image = cv2.drawContours(image, contours, c, (0, 255, 0), 3)
-    c += 1
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Image", gray)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    cv2.imshow("Image", blur)
+    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+    cv2.imshow("Image", thresh)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-left_upper = []
-right_upper = []
-left_lower = []
-right_lower = []
-left_upper_sum = 1000000000
-right_upper_sum = 0
-left_lower_sum = 0
-right_lower_sum = 0
+    max_area = 0
+    c = 0
+    best_cnt = contours[0]
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 10000:
+            if area > max_area:
+                max_area = area
+                best_cnt = i
+                image = cv2.drawContours(image, contours, c, (0, 255, 0), 3)
+        c += 1
 
-for i in best_cnt:
-    x, y = i[0][0], i[0][1]
-    if x + y > right_lower_sum:
-        right_lower_sum = x + y
-        right_lower = [x, y]
-    if x + y < left_upper_sum:
-        left_upper_sum = x + y
-        left_upper = [x, y]
-    if x - y < left_lower_sum:
-        left_lower_sum = x - y
-        left_lower = [x, y]
-    if y - x < right_upper_sum:
-        right_upper_sum = y - x
-        right_upper = [x, y]
+    right_upper, left_lower, right_lower, left_upper = get_corners(best_cnt)
+
+    width, height = 400, 400
+    pts1 = np.float32([left_upper, right_upper, left_lower, right_lower])
+    pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+
+    imgWarped = cv2.warpPerspective(image, matrix, (width, height))
+    imgWarped = ~imgWarped  # invert colors
+    results = get_sudoku_tiles(imgWarped, width)
+    # solve(results, width, height)
 
 
-width, height = 400, 400
-pts1 = np.float32([left_upper, right_upper, left_lower, right_lower])
-pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
-matrix = cv2.getPerspectiveTransform(pts1, pts2)
+def solve(results, width, height):
+    sudoku_solver = Model("./sudoku-solver.mzn")
+    gecode = Solver.lookup("gecode")
+    instance = Instance(gecode, sudoku_solver)
+    instance['board'] = results
 
-imgWarped = cv2.warpPerspective(image, matrix, (width, height))
-imgWarped = ~imgWarped  # invert colors
+    result = instance.solve()
+    solution = result["puzzle"]
 
-step = width // 9
-sudoku = [[None for _ in range(9)] for _ in range(9)]
+    sudoku_clean = cv2.imread("sudoku_clean.png")
+    sudoku_clean = cv2.resize(sudoku_clean, (width, height))
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    step = width // 9
+    fontScale = 1
+    color = (0, 0, 0)
+    thickness = 1
+    for i in range(9):
+        i_err = 0
+        if i > 2:
+            i_err = 2
+        if i > 5:
+            i_err = 4
+        for j in range(9):
+            pos = (14 + i * step + i_err, 32 + j * step + i_err)
+            sudoku_clean = cv2.putText(sudoku_clean, str(solution[j][i]), pos, font,
+                                       fontScale, color, thickness, cv2.LINE_AA)
 
-for i in range(9):
-    basic_err = 5
-    i_err = basic_err
-    if i > 2:
-        i_err = 6
-    if i > 5:
-        i_err = 7
-    for j in range(9):
-        sudoku[i][j] = imgWarped[0 + i * step + i_err: (i + 1)*step+i_err-2*basic_err, 0 + j * step + i_err: (j + 1)*step+i_err-2*basic_err]
-        # cv2.imshow(f"cell {i+1} {j+1}", imgWarped[0 + i*step + i_err: (i+1)*step, 0 + j*step + i_err: (j+1)*step])
-
-
-# TENSORFLOW
-path = Path()
-
-pathlib.PosixPath = pathlib.WindowsPath
-
-learn_inf = load_learner(path/'sudoku_neural_network.pkl')
-
-results = [[0 for _ in range(9)] for _ in range(9)]
-
-for i in range(9):
-    for j in range(9):
-        pred, _, probs = learn_inf.predict(sudoku[i][j])
-        pred = int(pred)
-        results[i][j] = pred
-        if probs[pred].item() < 0.80:
-            results[i][j] = 0
-
-print(results)
-
-# MINIZINC
-
-sudoku_solver = Model("./sudoku-solver.mzn")
-gecode = Solver.lookup("gecode")
-instance = Instance(gecode, sudoku_solver)
-instance['board'] = [[0,  0,  4,  0,  5,  0,  0,  0,  0],
-                     [9,  0,  0,  7,  3,  4,  6,  0,  0],
-                     [0,  0,  3,  0,  2,  1,  0,  4,  9],
-                     [0,  3,  5,  0,  9,  0,  4,  8,  0],
-                     [0,  9,  0,  0,  0,  0,  0,  3,  0],
-                     [0,  7,  6,  0,  1,  0,  9,  2,  0],
-                     [3,  1,  0,  9,  7,  0,  2,  0,  0],
-                     [0,  0,  9,  1,  8,  2,  0,  0,  3],
-                     [0,  0,  0,  0,  6,  0,  1,  0,  0]]
-
-result = instance.solve()
-solution = result["puzzle"]
+    cv2.imshow("Final Image", sudoku_clean)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-sudoku_clean = cv2.imread("sudoku_clean.png")
-sudoku_clean = cv2.resize(sudoku_clean, (width, height))
-font = cv2.FONT_HERSHEY_SIMPLEX
-fontScale = 1
-color = (0, 0, 0)
-thickness = 1
-for i in range(9):
-    i_err = 0
-    if i > 2:
-        i_err = 2
-    if i > 5:
-        i_err = 4
-    for j in range(9):
-        pos = (14 + i * step + i_err, 32 + j * step + i_err)
-        sudoku_clean = cv2.putText(sudoku_clean, str(solution[j][i]), pos, font,
-                            fontScale, color, thickness, cv2.LINE_AA)
+def get_corners(best_cnt):
+    left_upper = []
+    right_upper = []
+    left_lower = []
+    right_lower = []
+    left_upper_sum = 1000000000
+    right_upper_sum = 0
+    left_lower_sum = 0
+    right_lower_sum = 0
+
+    for i in best_cnt:
+        x, y = i[0][0], i[0][1]
+        if x + y > right_lower_sum:
+            right_lower_sum = x + y
+            right_lower = [x, y]
+        if x + y < left_upper_sum:
+            left_upper_sum = x + y
+            left_upper = [x, y]
+        if x - y < left_lower_sum:
+            left_lower_sum = x - y
+            left_lower = [x, y]
+        if y - x < right_upper_sum:
+            right_upper_sum = y - x
+            right_upper = [x, y]
+
+    return right_upper, left_lower, right_lower, left_upper
+
+def get_sudoku_tiles(img, width):
+    step = width // 9
+    sudoku = [[None for _ in range(9)] for _ in range(9)]
+
+    for i in range(9):
+        basic_err = 5
+        i_err = basic_err
+        if i > 2:
+            i_err = 6
+        if i > 5:
+            i_err = 7
+        for j in range(9):
+            sudoku[i][j] = img[0 + i * step + i_err: (i + 1) * step + i_err - 2 * basic_err,
+                           0 + j * step + i_err: (j + 1) * step + i_err - 2 * basic_err]
+            cv2.imshow(f"cell {i + 1} {j + 1}",
+                       img[0 + i * step + i_err: (i + 1) * step, 0 + j * step + i_err: (j + 1) * step])
+
+    path = Path()
+
+    pathlib.PosixPath = pathlib.WindowsPath
+
+    learn_inf = load_learner(path / 'nn_blank_images.pkl')
+
+    results = [[0 for _ in range(9)] for _ in range(9)]
+
+    for i in range(9):
+        for j in range(9):
+            pred, _, probs = learn_inf.predict(sudoku[i][j])
+            pred = int(pred)
+            results[i][j] = pred
+
+    return results
 
 
-cv2.imshow("Final Image", sudoku_clean)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+main()
