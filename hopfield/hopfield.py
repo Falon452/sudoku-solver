cb25468949f91b90c5ae27fb4681022861e60402
bblachut1 <bogojedi1@gmail.com>
2022-05-25 15:07:55 +0200

    delete useless lines, model for 2 images

--- hopfield/hopfield.py
+++ hopfield/hopfield.py
@@ -10,53 +10,12 @@ import cv2
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
 from hopfieldnetwork import HopfieldNetwork
 from hopfieldnetwork import images2xi
 
-IMAGE_SIZE = (30, 30, 3)
 TEST_PATH = './data/test/*.png'
 TRAIN_PATH = './data/train/*.png'
-
-
-def convert_to_black_and_white(name):
-    img = Image.open(name)
-    thresh = 200
-    fn = lambda x: 255 if x > thresh else 0
-    r = img.convert('L').point(fn, mode='1')
-    r.save('out2.png')
-
-
-from PIL import Image
-
-
-def img2array(name):
-    convert_to_black_and_white(name)
-    img = cv2.imread("out2.png")
-    img = cv2.resize(img, (IMAGE_SIZE[0], IMAGE_SIZE[1]), interpolation=cv2.INTER_AREA)
-    img = img.flatten()
-    binary_image = []
-    for i in img:
-        if i > 230:
-            binary_image.append(1)
-        else:
-            binary_image.append(0)
-
-    return binary_image
-
-
-def array2img(array):
-    array[array == -1] = 0
-    array *= 255
-    img = np.reshape(array, IMAGE_SIZE)
-    cv2.imshow("array2img", img)
-    cv2.waitKey(0)
-    return img
-
-
-def array2float(array):
-    tmp = np.asfarray(array)
-    tmp[tmp == 0] = -1.0
-    return tmp
 
 
 def show_images(images):
@@ -87,10 +46,9 @@ def xi_to_PIL(xi, N):
 
 if __name__ == '__main__':
     N = 100**2
-    iterations = 5
+    iterations = 10
     target = []
     for file in glob.glob(TRAIN_PATH):
-        # array = img2array(file)
         if file not in ("./data/train\shrek_color.png", "./data/train\\fiona_color.png"):
             continue
         target.append(file)
@@ -103,19 +61,13 @@ if __name__ == '__main__':
     network.save_network("./hopefield_network.npz")
 
     print("TESTING")
-    # network.load_network("./hopefield_network.npz")
+    network.load_network("./hopefield_network.npz")
 
     img_test = images2xi(['./data/test/fiona_color.png'], N)
-    half_image = np.copy(img_test)
-    half_image[: int(N / (100 / 50))] = -1
 
     network.set_initial_neurons_state(np.copy(img_test[:, 0]))
-    # network.set_initial_neurons_state(np.copy(half_image[:, 0]))
     network.update_neurons(iterations, 'async')
     test_pil = xi_to_PIL(img_test[:, 0], N)
-    # test_pil = xi_to_PIL(half_image, N)
     res_pil = xi_to_PIL(network.S, N)
 
-    # out_image = array2img(out[0]) #output network image
-    # img_test = array2img(test) #test image
     show_images([test_pil, res_pil])
