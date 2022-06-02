878f9185532e22e5a54468d01941cf4640e14c15
Falon <falon4522@gmail.com>
2022-06-02 19:34:22 +0200

    added mse

--- hopfield/hopfield.py
+++ hopfield/hopfield.py
@@ -10,12 +10,14 @@ import cv2
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
+from random import randint
 from PIL import Image
 from hopfieldnetwork import HopfieldNetwork
 from hopfieldnetwork import images2xi
 
 TEST_PATH = './data/test/*.png'
 TRAIN_PATH = './data/train/*.png'
+REF_PATH = './data/reference/*.png'
 
 
 def show_images(images):
@@ -26,6 +28,7 @@ def show_images(images):
 
     ax = fig.add_subplot(1, 2, 2)
     plt.imshow(images[1])
+
     ax.set_title('Result')
 
     plt.show()
@@ -44,13 +47,80 @@ def xi_to_PIL(xi, N):
     return im
 
 
-if __name__ == '__main__':
-    N = 100**2
-    iterations = 10
-    target = []
+def noise_image(file, percent):
+    print(file)
+
+    img = Image.open(file).convert('L')
+    img = np.array(img)
+    w, h = img.shape
+    for i in range(w):
+        for j in range(h):
+            if randint(0, 100) < percent:
+                img[i][j] = 255 - img[i][j]
+    img = Image.fromarray(img)
+
+    file = file.split(".png")[0]
+    file = file.split("\\")[1]
+    filename = ".\\data\\test\\" + file + str(percent) + ".png"
+
+    img.save(filename)
+
+
+def get_train_images_as_reference():
     for file in glob.glob(TRAIN_PATH):
-        if file not in ("./data/train\shrek_color.png", "./data/train\\fiona_color.png"):
-            continue
+        print(file)
+        img_test = images2xi([file], N)
+        network.set_initial_neurons_state(np.copy(img_test[:, 0]))
+        network.update_neurons(iterations, 'sync')
+
+        test_pil = xi_to_PIL(img_test[:, 0], N)
+        res_pil = xi_to_PIL(network.S, N)
+
+        file = file.split(".png")[0]
+        file = file.split("\\")[1]
+        filename = ".\\data\\reference\\" + file + ".png"
+        test_pil.save(filename)
+
+
+def predict(res_pil):
+    mses = {}
+    res_pil.save("result.png")
+    res_pil = Image.open("result.png").convert('L')
+    for file in glob.glob(REF_PATH):
+        reference_img = Image.open(file).convert('L')
+        mses[file] = mse_img(res_pil, reference_img)
+
+    lowest_mse = float('inf')
+    lowest_name = None
+    for filename, mse in mses.items():
+        if mse < lowest_mse:
+            lowest_mse = mse
+            lowest_name = filename
+
+    lowest_name = lowest_name.split("\\")[1]
+    lowest_name = lowest_name.split(".png")[0]
+    lowest_name = lowest_name.split("_")[0]
+    print(f"Predykcja -> {lowest_name.capitalize()} z mse={lowest_mse}")
+    return mses
+
+
+def mse_img(img1, img2):
+    img1, img2 = np.array(img1), np.array(img2)
+    sum = 0
+    w, h = img1.shape[:2]
+    for x in range(w):
+        for y in range(h):
+            diff = img1[x, y] - img2[x, y]
+            sum += diff
+    return sum / (w * h)
+
+
+if __name__ == '__main__':
+    N = 100 ** 2
+    iterations = 5
+    target = []
+
+    for file in glob.glob(TRAIN_PATH):
         target.append(file)
 
     target = images2xi(target, N)
@@ -60,14 +130,18 @@ if __name__ == '__main__':
     network.train_pattern(target)
     network.save_network("./hopefield_network.npz")
 
+
     print("TESTING")
     network.load_network("./hopefield_network.npz")
+    for file in glob.glob(TEST_PATH):
+        print(file)
+        img_test = images2xi([file], N)
+        network.set_initial_neurons_state(np.copy(img_test[:, 0]))
+        network.update_neurons(iterations, 'async')
 
-    img_test = images2xi(['./data/test/fiona_color.png'], N)
+        test_pil = xi_to_PIL(img_test[:, 0], N)
+        res_pil = xi_to_PIL(network.S, N)
 
-    network.set_initial_neurons_state(np.copy(img_test[:, 0]))
-    network.update_neurons(iterations, 'async')
-    test_pil = xi_to_PIL(img_test[:, 0], N)
-    res_pil = xi_to_PIL(network.S, N)
+        predict(res_pil)
 
-    show_images([test_pil, res_pil])
+        show_images([test_pil, res_pil])
